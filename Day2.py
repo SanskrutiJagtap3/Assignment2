a43dfc719955196f6ef8909b8c4cc912a2222f17
SanskrutiJagtap3 <138582250+SanskrutiJagtap3@users.noreply.github.com>
2023-07-07 20:07:45 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,112 @@
+# arithmetic operators
+a=10
+b=5
+print("Arithmetic Operators!!")
+print("Addition is:", a+b)
+print("Subtraction is:",a-b)
+print("Multiplication is:",a*b)
+
+#assignment Operators
+c=10
+c+=1
+print("assignment Operators!!")
+print("+=",c)
+c-=1
+print("-=",c)
+c//=5
+print("//=",c)
+
+# comparision operators
+d=10
+e=5
+print("comparision operators!!")
+print("10>5:", d>e)
+print("10<5:",d<e)
+print("10==5:",d==e)
+print("10!=5",d!=e)
+
+#logical operators
+
+a=True
+b=False
+print("logical operators!!")
+print("a and b:",a and b)
+print("a or b:",a or b)
+print("not a is:",not a)
+
+
+#Bitwise Operators
+a=10
+b=20
+print("Bitwise Operators!!")
+print("a or b:",a|b)
+print("a and b:",a&b)
+print("a xor b:",a^b)
+
+#identity operators
+a=5
+b=6
+print("identity operators!!")
+print(a is b)
+print(a is not b)
+
+#membership operators
+a="sanskruti"
+print("membership operators!!")
+print("s" in a)
+print("S" not in a) 
+
+#char to ASCII ord()
+# a="A"
+# val=ord(a)
+# print(val)
+
+# #ASCII to char chr()
+# print(chr(val))
+
+#String formatting
+# a=10
+# b=20
+# sum=a+b  
+# print("The addition of ",a,"and",b, "is:",sum)
+# print("The addition of {} and {} is {}".format(a,b,sum))
+# print(f"The addition of {a} and {b} is {sum}")
+# print("The addition of %d and %d is %d"%(a,b,sum))
+
+#Control Flow 
+#if..elif 
+# a=int(input("Enter a Number:"))
+# if a>0:
+#     print("+ve")
+# elif a<0:
+#     print("-ve")
+# else:
+#     print("0")
+
+#nested if (largest)
+# a= int(input("Enter Numbers:"))
+# b= int(input())
+# c = int(input())
+# if a>=b:
+#   if a==b:
+#     print("Both are same!!")
+#   else:
+#     print(" a is greatest!!")
+#   if b>a:
+#     print("b is greatest!!")
+# else:
+#   print("c is greatest!!")
+
+#nested if (samllest)
+# a= int(input("Enter Numbers:"))
+# b= int(input())
+# c = int(input())
+# if a<=b:
+#   if a==b:
+#     print("Both are same!!")
+#   else:
+#     print(" a is samllest!!")
+#   if b<a:
+#     print("b is samllest!!")
+# else:
+#   print("c is samllest!!")
